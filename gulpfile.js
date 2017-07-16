2c10aa219424b8720fa5c37efc9d978522757910
Adam Pengh <adam.pengh@gmail.com>
2017-07-16 08:15:55 -0400

    update

--- gulpfile.js
+++ gulpfile.js
@@ -19,16 +19,16 @@ const autoprefixer  = require('gulp-autoprefixer');
 // ----------------------------------------------------------------
 const config        = require('./gulp.config.json');
 
-
-
+const reload        = browserSync.reload;
 
 // ================================================================
 // Clean Task
 // ================================================================
-gulp.task('clean', function(done) {
+function clean(done) {
   return del( config.paths.dest.css + '**/*' );
   done();
-});
+}
+exports.clean = clean;
 
 
 
@@ -36,13 +36,15 @@ gulp.task('clean', function(done) {
 // ================================================================
 // STYLES
 // ================================================================
-gulp.task('styles', function(done) {
+function styles(done) {
   return gulp.src( config.paths.src.scss + '/**/*.scss' )
     .pipe(sass( config.modules.sass ))
     .pipe(autoprefixer( config.modules.autoprefixer ))
-    .pipe(gulp.dest( config.paths.dest.css ));
+    .pipe(gulp.dest( config.paths.dest.css ))
+    .pipe(browserSync.stream());
   done();
-});
+}
+exports.styles = styles;
 
 
 
@@ -51,16 +53,31 @@ gulp.task('styles', function(done) {
 // SERVER
 // ================================================================
 // Static Server
-gulp.task('browser-sync', function() {
-  browserSync.init( "./" );
+gulp.task('browser-sync', function(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  done();
 });
 
+
+
+// ================================================================
+// WATCH
+// ================================================================
+function watch() {
+  gulp.watch(config.paths.src.scss + '/**/*.scss', styles);
+  gulp.watch("*.html").on("change", reload);
+}
+exports.watch = watch;
 
 
 
 // ================================================================
 // Main Tasks
 // ================================================================
-gulp.task('build', gulp.series('styles') );
-gulp.task('serve', gulp.series('build', 'browser-sync'));
+gulp.task('build', gulp.series(clean, styles) );
+gulp.task('serve', gulp.series('build', 'browser-sync', watch));
 gulp.task('default', gulp.series( 'build') );
